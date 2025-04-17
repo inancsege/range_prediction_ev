b1e409b01a0387210f59764819d24300d42ee2a1
serhat ege inanç <81191418+inancsege@users.noreply.github.com>
2025-04-17 05:24:24 +0300

    optimized and aligned with the utils and preprocess

--- linear_regression.py
+++ linear_regression.py
@@ -9,8 +9,13 @@ from sklearn.metrics import mean_squared_error as mse
 #%%
 #%%
 
-from Transormers.Range_Prediction_EV.utils.utils import load_data, plot_results
-from Transormers.Range_Prediction_EV.preprocess.preprocess import prepare_data
+from Transormers.Range_Prediction_EV.utils.utils import load_data
+from Transormers.Range_Prediction_EV.preprocess.preprocess import prepare_data, preprocess_data
+
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def train_and_evaluate(X, Y):
     X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state=1)
@@ -43,25 +48,25 @@ def main():
     data_enc_dum = load_data("../../Range_Prediction_EV/data_enc_dummies.csv")
 
     if data is not None:
-        X, Y = prepare_data(data, 'trip_distance(km)', ['fuel_note','manufacturer', 'model', 'version', 'odometer',
+        X, Y, df_clean = preprocess_data(data, ['trip_distance(km)'], ['fuel_note','manufacturer', 'model', 'version', 'odometer',
            'trip_distance(km)', 'fuel_type','tire_type', 'driving_style'])
         y_test, y_pred, mse_value, r2_value, cv_mse = train_and_evaluate_with_scaling(X, Y)
-        print(f"MSE for normal LR: {mse_value}, R2: {r2_value}, CV MSE: {cv_mse}")
-        plot_results(X, y_test, y_pred, "Normal")
+        logging.info(f"MSE for normal LR: {mse_value}, R2: {r2_value}, CV MSE: {cv_mse}")
+        plot_actual_vs_predicted(y_test, y_pred, "Normal")
 
     if data_enc_l is not None:
-        X_enc_l, Y_enc_l = prepare_data(data_enc_l, 'trip_distance(km)', ['fuel_note','manufacturer', 'model', 'version', 'odometer',
+        X_enc_l, Y_enc_l, df_clean_enc_l = preprocess_data(data_enc_l, ['trip_distance(km)'], ['fuel_note','manufacturer', 'model', 'version', 'odometer',
            'trip_distance(km)', 'fuel_type','tire_type', 'driving_style'])
         y_test_enc_l, y_pred_enc_l, mse_enc_l, r2_enc_l, cv_mse_enc_l = train_and_evaluate_with_scaling(X_enc_l, Y_enc_l)
-        print(f"MSE for label encoded LR: {mse_enc_l}, R2: {r2_enc_l}, CV MSE: {cv_mse_enc_l}")
-        plot_results(X_enc_l, y_test_enc_l, y_pred_enc_l, "Label Encoded")
+        logging.info(f"MSE for label encoded LR: {mse_enc_l}, R2: {r2_enc_l}, CV MSE: {cv_mse_enc_l}")
+        plot_actual_vs_predicted(y_test_enc_l, y_pred_enc_l, "Label Encoded")
 
     if data_enc_dum is not None:
-        X_enc_dum, Y_enc_dum = prepare_data(data_enc_dum, 'trip_distance(km)', ['fuel_note','manufacturer', 'model', 'version', 'odometer',
+        X_enc_dum, Y_enc_dum, df_clean_enc_dum = preprocess_data(data_enc_dum, ['trip_distance(km)'], ['fuel_note','manufacturer', 'model', 'version', 'odometer',
            'trip_distance(km)', 'fuel_type'])
         y_test_enc_dum, y_pred_enc_dum, mse_enc_dum, r2_enc_dum, cv_mse_enc_dum = train_and_evaluate_with_scaling(X_enc_dum, Y_enc_dum)
-        print(f"MSE for dummy encoded LR: {mse_enc_dum}, R2: {r2_enc_dum}, CV MSE: {cv_mse_enc_dum}")
-        plot_results(X_enc_dum, y_test_enc_dum, y_pred_enc_dum, "Dummy Encoded")
+        logging.info(f"MSE for dummy encoded LR: {mse_enc_dum}, R2: {r2_enc_dum}, CV MSE: {cv_mse_enc_dum}")
+        plot_actual_vs_predicted(y_test_enc_dum, y_pred_enc_dum, "Dummy Encoded")
 
 
 if __name__ == "__main__":
