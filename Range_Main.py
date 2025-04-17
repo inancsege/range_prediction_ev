a927299b806787325823464b94176a3625a41aad
serhat ege inanç <81191418+inancsege@users.noreply.github.com>
2025-04-17 04:45:21 +0300

    added matplotlib library

--- Range_Main.py
+++ Range_Main.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import numpy as np
+import matplotlib.pyplot as plt
 from Transormers.Range_Prediction_EV.utils.utils import load_data, preprocess_data, plot_actual_vs_predicted, plot_range_comparison
 from Transormers.Range_Prediction_EV.preprocess.preprocess import preprocess_data
 
